c8e59125acc39c4efce038290f89aa79b59e0314
Bohdan Kotsiuba <bohdan.kotsiuba@gmail.com>
2024-05-13 23:30:16 +0300

    BACK Move getUser to Utils

--- ws/src/main/java/com/github/arlekinside/diploma/ws/Utils.java
+++ ws/src/main/java/com/github/arlekinside/diploma/ws/Utils.java
@@ -1,5 +1,9 @@
 package com.github.arlekinside.diploma.ws;
 
+import com.github.arlekinside.diploma.data.entity.User;
+import com.github.arlekinside.diploma.ws.config.security.user.UserDetailsAdapter;
+import org.springframework.security.core.Authentication;
+
 public class Utils {
 
     public static <T> T nvl(T input, T def) {
@@ -7,6 +11,10 @@ public class Utils {
             return def;
         }
         return input;
+    }
+
+    public static User getUser(Authentication auth) {
+        return ((UserDetailsAdapter) auth.getPrincipal()).user();
     }
 
 }
--- ws/src/main/java/com/github/arlekinside/diploma/ws/controller/AbstractCrudController.java
+++ ws/src/main/java/com/github/arlekinside/diploma/ws/controller/AbstractCrudController.java
@@ -1,13 +1,13 @@
 package com.github.arlekinside.diploma.ws.controller;
 
 import com.github.arlekinside.diploma.data.entity.User;
-import com.github.arlekinside.diploma.ws.config.security.user.UserDetailsAdapter;
+import com.github.arlekinside.diploma.ws.Utils;
 import org.springframework.security.core.Authentication;
 
 public abstract class AbstractCrudController<T> implements CrudController<T> {
 
     protected User getUser(Authentication auth) {
-        return ((UserDetailsAdapter) auth.getPrincipal()).user();
+        return Utils.getUser(auth);
     }
 
 }
